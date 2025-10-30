34a02db0132aa6069e2f306e64587ead3e41c926
Isha <isha18919@gmail.com>
2025-10-30 16:57:41 +0530

    Add screenshots

--- Backend/obj/Debug/net8.0/StudentManagementSystem.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/StudentManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a004234eb78b63559a2791fdd76fa20d40d8d1f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcf88ef66e108c5284e665e7949b456f621724da")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
